23846e0d4c1b74a480b37e6c7475d2410e48be16
bella <maekawatakaya10.8@gmail.com>
2025-04-28 15:49:37 +0900

    successfully to deploy to qiita

--- deploy_to_qiita.sh
+++ deploy_to_qiita.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# public ディレクトリを削除して source/_posts の内容をコピー
 rm -rf ./public
 cp -r ./source/_posts/ ./public
 
-# public ディレクトリ内の各 Markdown ファイルを処理
 find ./public -type f -name '*.md' -exec bash -c '
   FILE="$1"
   FRONT_MATTER=$(head -n 999 "$FILE" | grep -E "^title:|tags:|abbrlink:|date:")
@@ -18,18 +16,16 @@ find ./public -type f -name '*.md' -exec bash -c '
   sed -i "6a ignorePublish: false" "$FILE"
 ' _ {} \;
 
-# # source/_posts 内の .md ファイルのベースネームを配列に格納
+# 個別処理したい場合は以下を使用
 # declare -a basenames
 # while IFS= read -r -d $'\0' file; do
 #   basename=$(basename "$file" .md)
 #   basenames+=("$basename")
 # done < <(find ./source/_posts -type f -name '*.md' -print0)
 
-# # 各ベースネームに対して qiita publish を実行
 # for basename in "${basenames[@]}"; do
 #   echo "Publishing: $basename.md"
 #   npx qiita publish "$basename"
 # done
 
-# Qiita に公開
-# npx qiita publish --all
+npx qiita publish --all
